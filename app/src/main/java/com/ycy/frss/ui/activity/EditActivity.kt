4bf407b2d216816404e6998af060ad59c3687150
Chengyu Yang <chengyu_yang@outlook.com>
2018-06-26 23:03:21 +0800

    Edit Activity

--- app/src/main/java/com/ycy/frss/ui/activity/EditActivity.kt
+++ app/src/main/java/com/ycy/frss/ui/activity/EditActivity.kt
@@ -1,8 +1,12 @@
 package com.ycy.frss.ui.activity
 
+import android.animation.LayoutTransition
 import android.os.Bundle
+import android.view.ViewGroup
 import com.ycy.baselibrary.ui.activity.BaseActivity
 import com.ycy.frss.R
+import com.ycy.frss.ui.fragment.EditFragment
+import com.ycy.frss.ui.fragment.PreviewFragment
 import com.ycy.markdown.Adapter.ToolsAdapter
 import com.ycy.markdown.Listener.OnPreInsertListener
 import com.ycy.markdown.Utils.MarkDownController
@@ -10,11 +14,28 @@ import com.ycy.markdown.View.MarkDownEditorView
 import com.ycy.markdown.View.MarkDownPreviewView
 
 class EditActivity : BaseActivity(), OnPreInsertListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
+        initToolBarTransition()
+        initFragments()
         initToolbar()
+    }
+
+    private fun initToolBarTransition() {
+        val appBarLayout = findViewById<ViewGroup>(R.id.mAppBar)
+        appBarLayout.layoutTransition.setDuration(LayoutTransition.CHANGE_DISAPPEARING, 0)
+
+        val viewGroup = findViewById<ViewGroup>(R.id.mEditRootView)
+        val layoutTransition = viewGroup.layoutTransition
+        layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+    }
+
+    private fun initFragments() {
+        mEditFragment = EditFragment.newInstance("123", "123")
+        mPreviewFragment = PreviewFragment.newInstance()
     }
 
     private fun initToolbar() {
@@ -59,5 +80,7 @@ class EditActivity : BaseActivity(), OnPreInsertListener {
         private var mPreviewView: MarkDownPreviewView? = null
         private var mMarkDownController: MarkDownController? = null
         private var mToolsAdapter: ToolsAdapter? = null
+        private var mEditFragment: EditFragment? = null
+        private var mPreviewFragment: PreviewFragment? = null
     }
 }
